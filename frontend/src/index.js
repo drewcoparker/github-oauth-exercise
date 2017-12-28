d72c432891a80ac0263a8e6568e170cfcb559b25
Drew Parker <drew.i.parker@gmail.com>
2017-12-28 15:58:27 -0500

    Created actionCreators.js file and renamed the reducer from index.js to masterReducer.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import reduxPromise from 'redux-promise';
-import reducers from './reducers/index.js';
+import reducers from './reducers/masterReducer.js';
 
 import { Route, Switch } from 'react-router'
 import { Redirect } from 'react-router-dom'
@@ -15,18 +15,20 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const history = createHistory();
+const history = createHistory()
 
 const authSuccess = () => ({
-  type: 'AUTH_SUCCESS'
+    type: 'AUTH_SUCCESS'
 })
 
 const authFail = () => ({
-  type: 'AUTH_FAIL'
+    type: 'AUTH_FAIL'
 })
 
 const initialState = {
-  isAuthenticated: false
+    isAuthenticated: false,
+    isLoading: false,
+    hasErrored: false
 }
 
 const store = createStore(
@@ -38,11 +40,10 @@ const store = createStore(
     )
 )
 
-// ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>,
     document.getElementById('root')
-);
-registerServiceWorker();
+)
+registerServiceWorker()
